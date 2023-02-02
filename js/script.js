5dd2e25d411523f8584ed8f9db313c307b98b447
hijazi <hijazizulkifli@gmail.com>
2023-02-02 12:29:30 +0800

    TweenMax test 5

--- js/script.js
+++ js/script.js
@@ -22,7 +22,8 @@ var myAnimation = new hoverEffect({
     speedOut: 1.5,
     image1: '../img/Trash.jpg',
     image2: '../img/WP.png',
-    displacementImage: '../img/14.jpg'
+    displacementImage: '../img/14.jpg',
+    TweenMax.to(".my-div", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -32,7 +33,8 @@ var myAnimation = new hoverEffect({
     speedOut: 1.5,
     image1: '../img/Farmer2 (1).jpeg',
     image2: '../img/WP2.png',
-    displacementImage: '../img/14.jpg'
+    displacementImage: '../img/14.jpg',
+    TweenMax.to(".my-div2", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -42,7 +44,8 @@ var myAnimation = new hoverEffect({
     speedOut: 1.5,
     image1: '../img/Photography.png',
     image2: '../img/WP3.png',
-    displacementImage: '../img/14.jpg'
+    displacementImage: '../img/14.jpg',
+    TweenMax.to(".my-div3", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -52,13 +55,12 @@ var myAnimation = new hoverEffect({
     speedOut: 1.5,
     image1: '../img/Portfolio.png',
     image2: '../img/WP4.png',
-    displacementImage: '../img/14.jpg'
+    displacementImage: '../img/14.jpg',
+    TweenMax.to(".my-div4", 2, {borderRadius: "50px"})
 });
 
-TweenMax.to(".my-div", 2, {borderRadius: "50px"});
 
-TweenMax.to(".my-div2", 2, {borderRadius: "50px"});
 
-TweenMax.to(".my-div3", 2, {borderRadius: "50px"});
 
-TweenMax.to(".my-div4", 2, {borderRadius: "50px"});
+
+

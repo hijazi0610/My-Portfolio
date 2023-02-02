29002774be32e40aa6016aa883a3f9179241d003
hijazi <hijazizulkifli@gmail.com>
2023-02-02 12:38:50 +0800

    Test 6

--- js/script.js
+++ js/script.js
@@ -23,7 +23,6 @@ var myAnimation = new hoverEffect({
     image1: '../img/Trash.jpg',
     image2: '../img/WP.png',
     displacementImage: '../img/14.jpg',
-    TweenMax.to(".my-div", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -34,7 +33,6 @@ var myAnimation = new hoverEffect({
     image1: '../img/Farmer2 (1).jpeg',
     image2: '../img/WP2.png',
     displacementImage: '../img/14.jpg',
-    TweenMax.to(".my-div2", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -45,7 +43,6 @@ var myAnimation = new hoverEffect({
     image1: '../img/Photography.png',
     image2: '../img/WP3.png',
     displacementImage: '../img/14.jpg',
-    TweenMax.to(".my-div3", 2, {borderRadius: "50px"})
 });
 
 var myAnimation = new hoverEffect({
@@ -56,10 +53,9 @@ var myAnimation = new hoverEffect({
     image1: '../img/Portfolio.png',
     image2: '../img/WP4.png',
     displacementImage: '../img/14.jpg',
-    TweenMax.to(".my-div4", 2, {borderRadius: "50px"})
 });
 
-
+var tween = new TweenMax.to(".my-div", 2, {borderRadius: "50px"});
 
 
 
